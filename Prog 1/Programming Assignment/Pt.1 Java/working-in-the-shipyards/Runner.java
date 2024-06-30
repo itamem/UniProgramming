5a41b6d11ac8eb2e50c4826074dc50f1ba2c3462
itamem <itismemustafa11@gmail.com>
2024-07-01 04:14:09 +1000

    init

---
+++ Prog 1/Programming Assignment/Pt.1 Java/working-in-the-shipyards/Runner.java
@@ -0,0 +1,6 @@
+public class Runner {
+    public static void main(String[] args) {
+        //You can use this to test and debug your code.
+       // System.out.println(shipSegments.length);
+    }
+}
---
+++ Prog 1/Programming Assignment/Pt.2 Java/foundations-of-cartography/Runner.java
@@ -0,0 +1,5 @@
+public class Runner {
+    public static void main(String[] args) {
+        //Just for your testing.
+    }
+}
